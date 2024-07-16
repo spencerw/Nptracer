3d4e0b9e907203a45999356549aa5c5f0b3c2155
astrochialinko <chialinko@gapp.nthu.edu.tw>
2024-07-16 17:38:40 -0500

    create changaLoader feature

---
+++ changaLoader.py
@@ -0,0 +1,46 @@
+import pynbody
+import glob as gl
+import pandas as pd
+import numpy as np
+from DataLoader import DataLoader
+
+class ChangaLoader(DataLoader):
+    def __init__(self, path_to_sim):
+        super().__init__()
+
+        self.path_to_sim = path_to_sim
+        self.dDelta = self.read_dDelta()
+        
+    def read_dDelta(self):
+        files_to_read = gl.glob(self.path_to_sim + '*.param')
+        with open(files_to_read[0], 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                if 'dDelta' in line:
+                    dDelta = float(line.split('=')[1])
+                    break
+        return dDelta
+        
+    def read_snaps(self):
+        files_to_read = gl.glob(self.path_to_sim + '*.*[0-9]')
+        alldata = []
+        
+        for i, fn in enumerate(files_to_read):
+            step_num = int(fn.split('/')[-1].split('.')[-1])
+            snap = pynbody.load(fn)
+            
+            id = snap['iord'].view(np.ndarray).reshape(-1, 1)
+            pos = snap['pos'].view(np.ndarray) # x, y, z
+            vel = snap['vel'].view(np.ndarray) # vx, vy, vz
+            mass = snap['mass'].view(np.ndarray).reshape(-1, 1)  
+            r = 2*snap['eps'].view(np.ndarray).reshape(-1, 1)          
+            t = step_num*self.dDelta*np.ones((len(snap), 1))
+            
+            snaps = np.concatenate((t, id, pos, vel, mass, r), axis=1)
+            df_snaps = pd.DataFrame(snaps, columns=self.columns)
+            alldata.append(df_snaps)
+            
+        df_alldata = pd.concat(alldata, ignore_index=True)
+        df_alldata.sort_values(by='t', inplace=True, ignore_index=True)
+
+        return df_alldata
