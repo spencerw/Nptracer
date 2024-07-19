a55c0a4d57f9e4b4dcfae5fcc4ada63b90e91163
Spencer Wallace <scw7@uw.edu>
2024-07-19 11:24:53 -0500

    Change import path in test_data.py

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,7 +1,7 @@
-import nptracer.gengaLoader
+from nptracer.gengaLoader import GengaLoader
 
 def test_data_shape():
-    loader = gengaLoader.GengaLoader('../simdata/genga/')
+    loader = GengaLoader('../simdata/genga/')
     snaps = loader.read_snaps()
 
     assert snaps.shape == (118, 10)
