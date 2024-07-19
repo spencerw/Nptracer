a06b70bfc8efec07d6d483f84c43a5cfc67c5f58
Spencer Wallace <scw7@uw.edu>
2024-07-19 11:26:36 -0500

    Fix file path for test_data_shape function

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,7 +1,7 @@
 from nptracer.gengaLoader import GengaLoader
 
 def test_data_shape():
-    loader = GengaLoader('../simdata/genga/')
+    loader = GengaLoader('simdata/genga/')
     snaps = loader.read_snaps()
 
     assert snaps.shape == (118, 10)
